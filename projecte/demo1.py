01cd81b1e88800025ae8687d804037e166297bdb
hjk <12454845@qq.com>
2021-01-22 10:39:08 +0800

    flask页面显示

--- projecte/demo1.py
+++ projecte/demo1.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
+#路由
 @app.route("/")
 def index():
-    return "dasdasdsadasdasa"
+    name="小兰"
+    sex="男"
+    age=15
+    return render_template( "index.html", **locals())
 @app.route("/<int:pk>")
 def detail(pk):
     return f"fghjkhsdgfjhgdsjfg{pk}"
+"""
+flask的模板文件
+"""
+
 if __name__ == '__main__':
     app.run(host="192.168.11.16",port="458",debug=True)
+
