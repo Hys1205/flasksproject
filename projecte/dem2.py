9d8178970b37e48f0eed98bc5bd3df97f8711e1b
hjk <12454845@qq.com>
2021-01-22 16:45:22 +0800

    flask页面显示内容

--- projecte/dem2.py
+++ projecte/dem2.py
@@ -3,6 +3,37 @@ flask的模板文件
 """
 from flask import Flask,render_template
 app=Flask(__name__)
-
+book={
+    "name":"斗破大陆",
+    "writer":"天蚕豆",
+    "actile":[
+        {
+            "id":1,
+            "title":"第一目录",
+            "concent":"斗气化马，恐怖如斯"
+        },
+        {
+            "id":2,
+            "title": "第二目录",
+            "concent": "斗气化马，恐怖如斯2"
+        },
+        {
+            "id":3,
+            "title": "第三目录",
+            "concent": "斗气化马，恐怖如斯3"
+        },
+    ]
+}
+@app.route("/")
+def botter():
+    actile=book["actile"]
+    return render_template("booter.html",**locals())
+@app.route("/<int:pk>")
+def footer(pk):
+    actile=None
+    for a in book["actile"]:
+        if a["id"]==pk:
+            actile=a
+    return render_template("footer.html",**locals())
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
