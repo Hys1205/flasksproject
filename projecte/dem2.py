47ec77cf7c1d47c29210a2ad17a15005676e5131
hjk <12454845@qq.com>
2021-01-23 17:50:58 +0800

    flask登入和登出

--- projecte/dem2.py
+++ projecte/dem2.py
@@ -1,8 +1,9 @@
 """
 flask的模板文件
 """
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect,request,url_for,session
 app=Flask(__name__)
+app.secret_key="123456"
 book={
     "name":"斗破大陆",
     "writer":"天蚕豆",
@@ -24,6 +25,13 @@ book={
         },
     ]
 }
+users=[
+{
+    "email":"123456789@qq.com",
+    "password":"123456789",
+}
+]
+
 
 @app.route("/")
 def botter():
@@ -36,13 +44,28 @@ def footer(pk):
         if a["id"]==pk:
             actile=a
     return render_template("footer.html",**locals())
-@app.route("/login")
+@app.route("/login",methods=["POST","GET"])
 def logins():
-    actile=book["actile"]
-    return render_template("login.html",**locals())
+    if request.method=="GET":
+        return render_template("login.html",**locals())
+    elif request.method=="POST":
+        user=None
+        email=request.form.get("email")
+        password=request.form.get("password")
+        for u in users:
+            if u["email"]==email and u["password"]==password:
+                session["user"]=email
+                return redirect(url_for("botter"))
+    # actile=book["actile"]
+    # return render_template("login.html",**locals())
 @app.route("/logto")
 def logtos():
     actile=book["actile"]
     return render_template("logto.html",**locals())
+
+@app.route("/logout")
+def logout():
+    session.pop("user")
+    return redirect(url_for("botter"))
 if __name__ == '__main__':
     app.run(debug=True)
